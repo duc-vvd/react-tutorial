416f6182de51e105c05b973a6cb1844c8fb84fec
VuVietDuc <vuvietduc9x@gmail.com>
2020-02-25 10:54:13 +0700

    create todo app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
+import tickImg from './img/tick.svg';
 
 class App extends Component {
   constructor(state) {
     super(state);
     this.state = {
+      newItem: '',
       todoItems : [
         {title: 'Đi đá bóng', isComplete: true}, 
         {title: 'Đi bơi', isComplete: true}, 
         {title: 'Đi tắm'}
       ]};
-    
-     
+      this.onKeyUp = this.onKeyUp.bind(this);
+      this.onChange = this.onChange.bind(this);
   }
 
   onItemClick(item) {
@@ -32,10 +34,36 @@ class App extends Component {
     }
   }
 
+  onKeyUp(event) {
+    if(event.keyCode === 13) {
+      let text = event.target.value;
+      if(!text) { return; }
+      text = text.trim();
+      if(!text) { return; }
+      this.setState({
+        newItem : '',
+        todoItems: [
+          {title: text, isComplete: false},
+          ...this.state.todoItems
+        ]
+      });
+    }
+  }
+
+  onChange(event) {
+    this.setState({
+      newItem : event.target.value
+    });
+  }
+
   render() {
-    const {todoItems} = this.state;
+    const {todoItems, newItem} = this.state;
     return(
       <div className="App">
+        <div className="App-header">
+          <img src={tickImg} width={32} height={32}/>
+          <input type="text" value={newItem} onChange={this.onChange} placeholder="Add a new item" onKeyUp={this.onKeyUp} />
+        </div>
         {
           todoItems.length > 0 && todoItems.map((item, index) => 
             <TodoItem 
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import './TodoItem.css';
 import classNames from 'classnames';
+import checkImg from '../img/check.svg';
+import checkCompleteImg from '../img/check-complete.svg';
 
 class TodoItem extends Component {
-
     render() {
         const {item, click} = this.props;// {item: item}={item: obj} or item = this.props.item
         
+        
+        let url = checkImg;
+        if(item.isComplete) {
+            url = checkCompleteImg;
+        }
+
         return(
-            <div onClick={click} className={classNames('TodoItem', {
+            <div className={classNames('TodoItem', {
                 'TodoItem-complete': item.isComplete === true
             })}>
+                <img onClick={click} src={url} width={32}/>
                 <p>{this.props.item.title}</p>
             </div>
         );
